282f36fbc687213e370b08ae1f0d2dda8c2a841b
Kirill Bolotov <k.y.bolotov@gmail.com>
2025-02-19 20:04:15 +0700

    Deploy services via Docker and Docker compose

--- services-implementation/manager/src/main.ts
+++ services-implementation/manager/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ManagerModule } from './app.module';
 
 process.env['WORKERS_COUNT'] = '1';
+process.env.PORT = '3000';
 
 async function bootstrap() {
   const app = await NestFactory.create(ManagerModule);
--- services-implementation/worker/src/main.ts
+++ services-implementation/worker/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkerModule } from './worker.module';
 
+process.env.PORT = '3001';
+
 async function bootstrap() {
   const worker = await NestFactory.create(WorkerModule);
   await worker.listen(process.env.PORT ?? 3001);
